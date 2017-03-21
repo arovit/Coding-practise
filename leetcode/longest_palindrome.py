0fd8a9b468ee66073c399b2674f60b37771bcd0e
Arovit Narula <Aro@Arovits-MacBook-Pro.local>
2017-03-21 15:35:39 -0700

    longest palindrome

--- leetcode/longest_palindrome.py
+++ leetcode/longest_palindrome.py
@@ -1,31 +1,40 @@
 #!/usr/bin/python
 
-class String:
-    def __init__(self, temp):
-        self.data = temp
-        h = w = len(temp) 
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        self.data = s
+        h = w = len(s)
         self.palinar = [ [0 for i in range(w)] for j in range(h) ]
         self.maxlen = 0
-      
-    def find_longest_palindrome(self):
+        self.palin = []
         for i in range(0, len(self.data)):    ## single char is a palindrome
-            self.palinar[i][i] = 1     
+            self.palinar[i][i] = 1
             self.maxlen = 1
+            self.palin = self.data[i]
 
         for i in range(0, len(self.data)-1):  ## check two consecutive chars
             if self.data[i] == self.data[i+1]:
-               self.palinar[i][i+1] = 1 
+               self.palinar[i][i+1] = 1
                self.maxlen = 2
-               
-         
-        for i in range(0, len(self.data)-1):    ## Now run a O(N2) loop - if (i+1) and (j-1) is pali
-            for j in range(i+1, len(self.data)):
-                if self.data[i+1][j-1] and (self.data[i] == self.data[j]):
-                    plen = j - i + 1 
-                    if plen > self.maxlen:
-                        self.maxlen = plen  
-            
+               self.palin = self.data[i:i+2]
+        #print self.data 
+        for i in range(3, len(self.data)+1):   ## Distance array
+            for j in range(0, len(self.data)-i+1):  ## Starting index
+                k = j + i - 1 
+                #print "%s %s - %s"%(j, k , i)
+                if self.palinar[j+1][k-1] and (self.data[j] == self.data[k]):
+                    #print "palin %s %s"%(j,k) 
+                    self.palinar[j][k] = 1 
+                    if i > self.maxlen:
+                        self.maxlen = i
+                        self.palin = self.data[j:k+1]
+          
+        return self.palin            
        
 
-temp = String("arovit") 
-temp.find_longest_palindrome()
+sol = Solution()
+print sol.longestPalindrome("cbbd")
