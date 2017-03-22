ac26a1f4c6450d3eda1d5bfa6d7cd3e20327355b
Arovit Narula <Aro@Arovits-MacBook-Pro.local>
2017-03-21 23:14:56 -0700

    Stack incomplete

---
+++ stacks_queues/stacks.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+
+class Stack:
+    def __init__(self):
+        self.top = None 
+    def push():
+        """ push the top of stack """ 
+      
+    def pop():
+        """ Pop the top of stack """ 
+    def peek():
+        """ Peek the top of stack """ 
