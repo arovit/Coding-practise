7ac6dcb3b6c227f68ec90df295bc10ee9f1f9d44
Arovit Narula <Aro@Arovits-MacBook-Pro.local>
2017-03-22 23:49:04 -0700

    ZigZag to be continue

--- leetcode/zigzag.py
+++ leetcode/zigzag.py
@@ -14,7 +14,20 @@ class Solution(object):
             fchar.append(data[njump])
             njump = njump + 2*(numRows-2) + 2   
         print ''.join(fchar) 
+     
+        schar = [] 
+        njump = 1
+        while njump < len(data):
+            schar.append(data[njump])
+            njump = njump + 2*(numRows-3) + 2  
+        print ''.join(schar)
 
+        lchar = [] 
+        njump = 2
+        while (njump > 0) and (njump < len(data)):
+            lchar.append(data[njump])
+            njump = njump + 2*(numRows-2) + 2
+        print ''.join(lchar)
 
 sol = Solution()
 sol.convert("arovit", 3) 
