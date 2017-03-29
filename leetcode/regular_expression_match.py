7d2b9af3ca92bec76ddc2f779b677a1228083288
arovit <arovit.kv@gmail.com>
2017-03-28 17:42:47 -0700

    regular expression implementation

---
+++ leetcode/regular_expression_match.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        for i in range(p.split()):
+                
