61ab2e6f724b260a3c318bd2cd1b5b974fab16d4
arovit <arovit.kv@gmail.com>
2017-04-03 19:10:44 -0700

    3SUM solved

--- leetcode/3SUM.py
+++ leetcode/3SUM.py
@@ -5,29 +5,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        
-        hash_result = {}
-        result_list = []
-        if len(nums) < 3:
-            return result_list
-        for i in nums:
-            count = hash_result.get(i, 0)
-            count += 1
-            hash_result[i] = count
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                temp_sum = nums[i] + nums[j]
-                lookfor = -1 * temp_sum
-                if (hash_result.get(nums[i], 0) > 0 and \
-                        hash_result.get(nums[j], 0) > 0 and \
-                            hash_result.get(lookfor, 0) > 0):
-                    temp_array = [nums[i], nums[j], lookfor]
-                    result_list.append(temp_array)
-                    hash_result[nums[i]] -= 1
-                    hash_result[nums[j]] -= 1
-                    hash_result[lookfor] -= 1
-        return result_list
+        nums = sorted(nums)
+        result = []
+        hashed_entry = {}
+        for i in xrange(len(nums)):
+            start = i + 1 
+            end = len(nums)-1
+            if i > 0 and (nums[i] == nums[i-1]):
+                continue
+            while start < end:
+                if nums[i] + nums[start] + nums[end] == 0:
+                    entry="%s#%s#%s"%(nums[i], nums[start], nums[end])
+                    if not entry in hashed_entry:
+                        result.append([nums[i], nums[start], nums[end]])
+                        hashed_entry[entry] = 1
+                    end = end - 1   
+                elif nums[i] + nums[start] + nums[end] > 0:
+                    end = end - 1
+                else:
+                    start = start + 1
+        return result
 
 solver = Solution()
-input_list =  [-1, 0, 1, 2, -1, -4]
+input_list =  [0,0]*10000
 print solver.threeSum(input_list)
