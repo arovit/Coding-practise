523e023c28aefb0ae395fa1df5e275b4f814365d
Arovit Narula <Aro@Arovits-MacBook-Pro.local>
2017-04-04 19:22:37 -0700

    teemo attacking solved

--- leetcode/teemo-attacking.py
+++ leetcode/teemo-attacking.py
@@ -9,11 +9,20 @@ class Solution(object):
         """
         mduration = 0
         for index, val in enumerate(timeSeries):
-            if index != len(timeSeries): 
+            if index != len(timeSeries)-1: 
                 if (timeSeries[index+1] - timeSeries[index]) < duration:
                     mduration = mduration + (timeSeries[index+1] - timeSeries[index])
                 else:
-                    mduration = mduration + duration       
-        print mduration
+                    mduration = mduration + duration
+            elif index == (len(timeSeries) -1):
+                mduration = mduration + duration
+    
+        return mduration
 
-Solution()
+sol = Solution()
+inp = raw_input("Enter the timeSeries")
+ts = [int(x) for x in inp.strip().split(' ')]
+inp = raw_input("Enter the Duration")
+dur = int(inp.strip())
+print sol.findPoisonedDuration(ts, dur) 
+
