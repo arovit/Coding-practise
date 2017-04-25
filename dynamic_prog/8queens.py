d9c318aa6c42441f593d5cd2f84c071f50e99bd3
Arovit Narula <Aro@Arovits-MacBook-Pro.local>
2017-04-25 08:03:55 -0700

    8 queens

--- dynamic_prog/8queens.py
+++ dynamic_prog/8queens.py
@@ -2,28 +2,41 @@
 
 import copy 
 
+board_size = 8 
 board = []
-board = [[0 for i in range(8)] for j in range(8)]
+board = [[0 for i in range(board_size)] for j in range(board_size)]
 
 
 def eight_queen(index, board, plist):
-    newboard = copy.deepcopy(board) 
-    for i in range(8):
-        for j in range(8):
-            if board[i][j] == 0:  ## position available
-                plist[index] == (i,j)
-                cancel    
+    if index == board_size:
+        return True
+    newboard = copy.deepcopy(board)
+    for i in range(board_size):
+        for j in range(board_size):
+            if newboard[i][j] == 0:  ## position available
+                plist[index]=(i,j)
+                newboardpos = copy.deepcopy(newboard)
+                cancelpositions(newboardpos, i, j)
+                if eight_queen(index+1, newboardpos, plist):
+                    return True
+                
+    return False      
 
-
-
-def cancelpositions(board, x, y, minx, maxx, miny, maxy):
-    for i in range(8):
-        board[i][y] = 1    
+def cancelpositions(board, x, y):
+    for i in range(board_size):
+        board[i][y] = 1
         board[x][i] = 1
-        if minx <= x+i <=maxx:  
+        if 0 <= x+i < board_size:
+            if 0 <= y+i < board_size:  
+                board[x+i][y+i] = 1
+            if 0 <= y-i < board_size:  
+                board[x+i][y-i] = 1
+        if 0 <= x-i < board_size:
+            if 0 <= y+i < board_size:  
+                board[x-i][y+i] = 1
+            if 0 <= y-i < board_size:  
+                board[x-i][y-i] = 1
 
-        if minx <= x+i <=maxx:  
-            board[x+i][y+i] = 1
-        board[x+i][y-i] = 1
-        board[x-i][y-i] = 1
-        board[x-i][y+i] = 1
+queen_positions = [(),(),(),(),(),(),(),()]
+eight_queen(0, board, queen_positions)
+print queen_positions
