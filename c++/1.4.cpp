868d7c4197097d3c641e6d1a444e0c06aca8f9e9
Arovit Narula <Aro@Arovits-MacBook-Pro.local>
2017-04-26 08:48:10 -0700

    C++ primer add to repository

---
+++ c++/1.4.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main(){
+int a, b; 
+std::cout << "Enter a" << std::endl;  
+std::cin >> a;
+std::cout << "Enter b" << std::endl;  
+std::cin >> b;
+std::cout << "Product of" << a << " and " << b << " is " << a*b << std::endl;  
+}
