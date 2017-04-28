3f43c503bae594863bb92a706c4c8283ca5e554c
Arovit Narula <Aro@Arovits-MacBook-Pro.local>
2017-04-28 09:35:30 -0700

    Combinational sum

--- leetcode/combination_sum.py
+++ leetcode/combination_sum.py
@@ -9,9 +9,10 @@ class Solution(object):
         """
         self.resultlist = []
         print self.checksum(candidates, target, 0, 0, [])
+        print self.resultlist
+        return self.resultlist
     
     def checksum(self, listn, tsum, index, ways, tresult):
-        print tresult, tsum
         if tsum == 0:
             self.resultlist.append(tresult[:])
             return 1
@@ -19,14 +20,26 @@ class Solution(object):
             return 0
         for i in range(index, len(listn)):
             num = listn[i]
+            count = 1
             while num <= tsum:
-                tresult.append(num)
-                if self.checksum(listn, tsum-num, index+1, ways, tresult):
+                append_list(tresult, listn[i], count)
+                if self.checksum(listn, tsum-num, i+1, ways, tresult):
                     ways += 1
-                tresult.pop()
-                num = num << 1
+                pop_list(tresult, count)
+                count += 1
+                num = listn[i] * count
         return ways
+
+def append_list(lis, num, times):
+    while times > 0:
+        lis.append(num)
+        times -= 1
+
+def pop_list(lis, times):
+    while times > 0:
+        lis.pop()
+        times -= 1
 
 
 sol = Solution()
-sol.combinationSum([2, 3,6,7],7)
+sol.combinationSum([1,2],4)
